348f7d2809783993fe94af0443f26d3da69940c5
William <william@Williams-MacBook-Pro.local>
2020-09-03 13:48:52 -0700

    broken change file

---
+++ Change.java
@@ -0,0 +1,25 @@
+public class Change{
+	public static void main(String[] args)
+	{
+		System.out.println("ggs man");
+		// cents = 5;
+		
+		// int value = 0;
+
+		// public static int NumWaysChange (int cents, int value)
+		// {
+		// 	if (cents = 0)
+		// 	{
+		// 		return 1;
+		// 	}
+
+		// 	int[] centValue = {25, 10, 5, 1};
+		// 	int i = 1;
+		// 	while(int[value]*i <= cents)
+		// 	{
+		// 		NumWaysChange(cents - int[value]*i);
+		// 		i++;
+		// 	}
+		// }
+	}
+}
